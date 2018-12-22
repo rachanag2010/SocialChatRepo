732bc4197bc73af50ff71aec4fde4d717943fbb3
Rachana <rachanag2010@gmail.com>
2018-12-22 09:56:04 -0600

    dzfgdfb

--- src/pages/recent/recent.ts
+++ src/pages/recent/recent.ts
@@ -9,8 +9,66 @@ import { ChatDetailsPage } from '../chat-details/chat-details';
   templateUrl: 'recent.html',
 })
 export class RecentPage {
+  items;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.initializeItems();
+  }
+  initializeItems() {
+    this.items = [
+      'Amsterdam',
+      'Bogota',
+      'Buenos Aires',
+      'Cairo',
+      'Dhaka',
+      'Edinburgh',
+      'Geneva',
+      'Genoa',
+      'Glasglow',
+      'Hanoi',
+      'Hong Kong',
+      'Islamabad',
+      'Istanbul',
+      'Jakarta',
+      'Kiel',
+      'Kyoto',
+      'Le Havre',
+      'Lebanon',
+      'Lhasa',
+      'Lima',
+      'London',
+      'Los Angeles',
+      'Madrid',
+      'Manila',
+      'New York',
+      'Olympia',
+      'Oslo',
+      'Panama City',
+      'Peking',
+      'Philadelphia',
+      'San Francisco',
+      'Seoul',
+      'Taipeh',
+      'Tel Aviv',
+      'Tokio',
+      'Uelzen',
+      'Washington'
+    ];
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
   ionViewDidLoad() {
@@ -22,6 +80,7 @@ export class RecentPage {
     this.navCtrl.push(ChatDetailsPage);
   }
   
-
-
 }
+
+
+
